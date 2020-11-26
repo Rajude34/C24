c44e34c8d2fd439cf4dfcd76c9537854f33e340f
Rajude34 <72769515+Rajude34@users.noreply.github.com>
2020-11-26 18:58:15 -0500

    Add files via upload

---
+++ bird.js
@@ -0,0 +1,29 @@
+class Bird {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.3,
+          'density':1.0
+      }
+      this.body = Bodies.circle(x, y, 50, options);
+      //this.width = 50;
+      //this.height = 50;
+      this.r = 50
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      pos.x = mouseX
+      pos.y = mouseY
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill("red");
+      strokeWeight(5)
+      stroke("black")
+      ellipse(0, 0, this.r, this.r);
+      pop();
+    }
+  };
